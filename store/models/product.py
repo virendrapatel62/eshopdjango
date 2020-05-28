8da44f84bd0a43de3aa7818f9bbb026ed1f6bf1f
Virendra <patelvirendra62@gmail.com>
2020-05-28 13:50:32 +0530

    product model

---
+++ store/models/product.py
@@ -0,0 +1,7 @@
+from  django.db import  models
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    description = models.CharField(max_length=200 , default='')
+    image = models.ImageField(upload_to='products/')
