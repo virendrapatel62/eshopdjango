df767ea1a83ef5336d842f6e5d02a9a8d371660e
Virendra <patelvirendra62@gmail.com>
2020-05-28 17:38:29 +0530

    sample data

--- store/models/product.py
+++ store/models/product.py
@@ -6,5 +6,5 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description = models.CharField(max_length=200, default='' , null=True)
+    description = models.CharField(max_length=200, default='' , null=True , blank=True)
     image = models.ImageField(upload_to='uploads/products/')
