eae8ec12945066f3a8a1fe5b49b4f927ce0280c0
Virendra <patelvirendra62@gmail.com>
2020-05-28 14:14:59 +0530

    Foriegn Key

--- store/models/product.py
+++ store/models/product.py
@@ -1,7 +1,8 @@
 from  django.db import  models
-
+from .category import Category
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category , on_delete=models.CASCADE , default=1)
     description = models.CharField(max_length=200 , default='')
     image = models.ImageField(upload_to='uploads/products/')
