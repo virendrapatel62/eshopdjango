05782ecbe8137e6528965c6be47b25f8806a39e4
Virendra <patelvirendra62@gmail.com>
2020-05-29 23:25:55 +0530

    form

--- store/models/customer.py
+++ store/models/customer.py
@@ -1,5 +1,5 @@
 from django.db import  models
-from django.core.validators import  MinLengthValidator
+from django.core.validators import MinLengthValidator
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
--- store/views.py
+++ store/views.py
@@ -33,12 +33,47 @@ def signup(request):
         phone = postData.get('phone')
         email = postData.get('email')
         password = postData.get('password')
-        print(first_name, last_name, phone, email, password)
-        customer = Customer(first_name=first_name,
-                            last_name=last_name,
-                            phone=phone,
-                            email=email,
-                            password=password)
+        # validation
+        value = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'phone': phone,
+            'email': email
+        }
+        error_message = None
 
-        customer.register()
-        return HttpResponse("Signup Success")
+        if (not first_name):
+            error_message = "First Name Required !!"
+        elif len(first_name) < 4:
+            error_message = 'First Name must be 4 char long or more'
+        elif not last_name:
+            error_message = 'Last Name Required'
+        elif len(last_name) < 4:
+            error_message = 'Last Name must be 4 char long or more'
+        elif not phone:
+            error_message = 'Phone Number required'
+        elif len(phone) < 10:
+            error_message = 'Phone Number must be 10 char Long'
+        elif len(password) < 6:
+            error_message = 'Password must be 6 char long'
+        elif len(email) < 5:
+            error_message = 'Email must be 5 char long'
+
+        # saving
+        if not error_message:
+            print(first_name, last_name, phone, email, password)
+            customer = Customer(first_name=first_name,
+                                last_name=last_name,
+                                phone=phone,
+                                email=email,
+                                password=password)
+    
+            customer.register()
+
+            return render(request ,'index.html')
+        else:
+            data =  {
+                'error': error_message,
+                'values': value
+            }
+            return render(request, 'signup.html', data)
