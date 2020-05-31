2cbf35972a9d5553bec649cf40bad2a1e171e6ad
Virendra <patelvirendra62@gmail.com>
2020-05-31 16:04:18 +0530

    quanityt

--- store/templatetags/cart.py
+++ store/templatetags/cart.py
@@ -9,3 +9,12 @@ def is_in_cart(product  , cart):
         if int(id) == product.id:
             return True
     return False;
+
+
+@register.filter(name='cart_quantity')
+def cart_quantity(product  , cart):
+    keys = cart.keys()
+    for id in keys:
+        if int(id) == product.id:
+            return cart.get(id)
+    return 0;
