94d854a190ead7ef82d7598edd2f5239fb7bdb19
Virendra <patelvirendra62@gmail.com>
2020-06-01 09:10:30 +0530

    middleware

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ from .views.login import Login , logout
 from .views.cart import Cart
 from .views.checkout import CheckOut
 from .views.orders import OrderView
-
+from .middlewares.auth import  auth_middleware
 
 
 urlpatterns = [
@@ -14,8 +14,7 @@ urlpatterns = [
     path('signup', Signup.as_view(), name='signup'),
     path('login', Login.as_view(), name='login'),
     path('logout', logout , name='logout'),
-path('cart', Cart.as_view() , name='cart'),
-path('check-out', CheckOut.as_view() , name='checkout'),
-path('orders', OrderView.as_view(), name='orders'),
-
+    path('cart', Cart.as_view() , name='cart'),
+    path('check-out', CheckOut.as_view() , name='checkout'),
+    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
 ]
--- store/views/orders.py
+++ store/views/orders.py
@@ -7,11 +7,10 @@ from django.views import View
 from store.models.product import Product
 from store.models.orders import Order
 from store.middlewares.auth import auth_middleware
-from django.utils.decorators import method_decorator
 
 class OrderView(View):
 
-    @method_decorator(auth_middleware)
+
     def get(self , request ):
         customer = request.session.get('customer')
         orders = Order.get_orders_by_customer(customer)
